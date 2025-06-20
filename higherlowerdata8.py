149b5459e0116428a512596e932f7b62f8069f68
Ampolu Mohan sai <105854251+mohansai279@users.noreply.github.com>
2025-06-21 03:03:19 +0530

    Add files via upload

---
+++ higherlowerdata8.py
@@ -0,0 +1,44 @@
+data = [
+    {
+        'name': "Ampolu Mohan Sai",
+        'follower_Count': 1,
+        'description': "YouTuber",
+        'country': "India"
+    },
+    {
+        'name': "Cristiano Ronaldo",
+        'follower_Count': 620,
+        'description': "Footballer",
+        'country': "Portugal"
+    },
+    {
+        'name': "Selena Gomez",
+        'follower_Count': 430,
+        'description': "Singer and Actress",
+        'country': "USA"
+    },
+    {
+        'name': "Elon Musk",
+        'follower_Count': 180,
+        'description': "Entrepreneur",
+        'country': "USA"
+    },
+    {
+        'name': "Narendra Modi",
+        'follower_Count': 100,
+        'description': "Prime Minister ",
+        'country': "India"
+    },
+    {
+        'name': "Virat Kohli",
+        'follower_Count': 265,
+        'description': "Cricketer",
+        'country': "India"
+    },
+    {
+        'name': "Kylie Jenner",
+        'follower_Count': 400,
+        'description': "Model and Influencer",
+        'country': "USA"
+    }
+]
